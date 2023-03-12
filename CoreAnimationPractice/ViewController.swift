2a630a5e20bc8db3c306d72418c2f628faec6d86
shenqing <shenqin1@msu.edu>
2023-03-12 16:11:27 -0400

    Basic animation to change a view's location.

--- CoreAnimationPractice/ViewController.swift
+++ CoreAnimationPractice/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let redView = UIView(frame: CGRect(x: 20, y: 100, width: 140, height: 100))
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redView.backgroundColor = .systemRed
+        view.addSubview(redView)
+        
+        animation()
     }
 
-
+    func animation(){
+        let animation = CABasicAnimation()
+        animation.keyPath = "position.x"
+        animation.fromValue = 20 + 140/2
+        animation.toValue = 300
+        animation.duration = 1
+        
+        redView.layer.add(animation, forKey: "basic")
+        redView.layer.position = CGPoint(x: 300, y: 100 + 100/2) // update to the final position
+    }
 }
 
