38caf9e369ec1529b9d4f87ff9897da1545e1cd3
shenqing <shenqin1@msu.edu>
2023-03-12 16:50:05 -0400

    Scale animation for another view

--- CoreAnimationPractice/ViewController.swift
+++ CoreAnimationPractice/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
     let redView = UIView(frame: CGRect(x: 20, y: 100, width: 140, height: 100))
+    
+    let anotherView = UIView()
+    let _width: CGFloat = 200
+    let _height:CGFloat = 100
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +21,18 @@ class ViewController: UIViewController {
         view.addSubview(redView)
         
         animation()
+        anotherView.backgroundColor = .systemBlue
+        view.addSubview(anotherView)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        anotherView.frame = CGRect(x: view.bounds.midX - _width/2,// Move to the center dynamically
+                                   y: view.bounds.midY - _height/2,
+                                   width: _width,
+                                   height: _height)
+        
+        scale()
     }
 
     func animation(){
@@ -28,6 +44,17 @@ class ViewController: UIViewController {
         
         redView.layer.add(animation, forKey: "basic")
         redView.layer.position = CGPoint(x: 300, y: 100 + 100/2) // update to the final position
+    }
+    
+    func scale(){
+        let animation = CABasicAnimation()
+        animation.keyPath = "transform.scale"
+        animation.fromValue = 1
+        animation.toValue = 2
+        animation.duration = 0.4
+        
+        anotherView.layer.add(animation, forKey: "scale")
+        anotherView.layer.transform = CATransform3DMakeScale(2, 2, 1)
     }
 }
 
