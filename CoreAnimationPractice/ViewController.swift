63b0dc7a209c1516f12e162be3fdd57ba7e33c4b
shenqing <shenqin1@msu.edu>
2023-03-14 22:41:32 -0400

    Add a textfield on user interface to put command for animation, add a button to execute it. Add IBOutlet and IBAction for the textfield and button, using the switch to know what kind of animation to do.

--- CoreAnimationPractice/ViewController.swift
+++ CoreAnimationPractice/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var animateTextField: UITextField!
+    
+    
     let redView = UIView(frame: CGRect(x: 20, y: 100, width: 140, height: 100))
     
     let anotherView = UIView()
@@ -28,8 +31,8 @@ class ViewController: UIViewController {
         view.addSubview(redView)
         
         animation()
-//        anotherView.backgroundColor = .systemBlue
-//        view.addSubview(anotherView)
+        anotherView.backgroundColor = .systemBlue
+        view.addSubview(anotherView)
         
         squareView.backgroundColor = .systemPink
         view.addSubview(squareView)
@@ -38,10 +41,10 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-//        anotherView.frame = CGRect(x: view.bounds.midX - _width/2,// Move to the center dynamically
-//                                   y: view.bounds.midY - _height/2,
-//                                   width: _width,
-//                                   height: _height)
+        anotherView.frame = CGRect(x: view.bounds.midX - _width/2,// Move to the center dynamically
+                                   y: view.bounds.midY - _height/2,
+                                   width: _width,
+                                   height: _height)
         squareView.frame = CGRect(x: view.bounds.midX - _sWidth/2
                                   , y: view.bounds.midY - _sHeight/2
                                   , width: _sWidth
@@ -61,10 +64,6 @@ class ViewController: UIViewController {
             ctx.cgContext.drawPath(using: .fillStroke)
         }
         redCircle.image = img
-//        scale()
-//        rotate()
-//        shake()
-        round()
     }
 
     func animation(){
@@ -77,6 +76,33 @@ class ViewController: UIViewController {
         redView.layer.add(animation, forKey: "basic")
         redView.layer.position = CGPoint(x: 300, y: 100 + 100/2) // update to the final position
     }
+    
+    @IBAction func animate(_ sender: UIButton) {
+        if let command = animateTextField.text{
+            switch command{
+            case "scale":
+                scale()
+            case "rotate":
+                rotate()
+            case "shake":
+                shake()
+            case "round":
+                round()
+            default:
+                let alertView = UIAlertController(title: "No such command.", message: "Please put a valid command", preferredStyle: .alert)
+                let alertAction = UIAlertAction(title: "Alright", style: .cancel, handler: nil)
+                alertView.addAction(alertAction)
+                present(alertView, animated: true, completion: nil)
+            }
+        }
+        else{
+            let alertView = UIAlertController(title: "No commands!", message: "Please put a command in text field.", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+            alertView.addAction(alertAction)
+            present(alertView, animated: true, completion: nil)
+        }
+    }
+    
     
     func scale(){
         let animation = CABasicAnimation()
